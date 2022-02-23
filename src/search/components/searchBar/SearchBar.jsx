afd2e8c8f9df63c27cf08bd5cec69e5a0e2660d3
smooff <smo.offchannel@gmail.com>
2022-02-23 18:02:11 +0100

    fix: opravenie pagination - opravenie chyb, ktore nastavali pri zmene poctu/zmene strany vyhladavanych vysledkov

--- src/search/components/searchBar/SearchBar.jsx
+++ src/search/components/searchBar/SearchBar.jsx
@@ -14,10 +14,10 @@ const SearchBar = (props) => {
     const useStyles = makeStyles(() => ({
         searchButton: {
             marginTop: "10px",
-            marginBottom:"10px",
+            marginBottom: "10px",
         },
-        dataTreeStyles:{
-            paddingBottom:"100px",
+        dataTreeStyles: {
+            paddingBottom: "100px",
         }
     }));
     const classes = useStyles();
@@ -29,12 +29,19 @@ const SearchBar = (props) => {
     const [pageCount, setPageCount] = React.useState(0);
     //pocet poloziek na page
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    //pri zmene stran vyhladanych vysledkov sa nastavuje pocet zaznamov zo statu
     const handleChangePage = (event, newPage) => {
-        setPageCount(newPage);
+        const count = newPage;
+        setPageCount(count);
+        handleSearch(rowsPerPage, count);
     };
+    //pri zmene poctu zaznamov sa nastavuje strana na prvu (0)
     const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPageCount(0);
+        const rows = parseInt(event.target.value, 10);
+        const count = 0;
+        setRowsPerPage(rows);
+        setPageCount(count);
+        handleSearch(rows, count);
     };
 
     //auth
@@ -44,17 +51,17 @@ const SearchBar = (props) => {
     const config = {
         headers: {Authorization: `Bearer ${authState.accessToken.accessToken}`}
     };
-    const bodyParameters = {
-        name: "",
-        pagingFilter: {
-            pageSize: rowsPerPage,
-            page: pageCount
-        }
-    };
-    const makeRequest = () => {
+
+    const makeRequest = (changeableSearchedValue, rows, count) => {
         return axios.put(
             'https://tp2-ai.fei.stuba.sk:8080/core/search/general',
-            bodyParameters,
+            {
+                name: changeableSearchedValue,
+                pagingFilter: {
+                    pageSize: rows,
+                    page: count
+                }
+            },
             config
         ).then((res) => {
             setResponseData("");
@@ -65,7 +72,7 @@ const SearchBar = (props) => {
     //handle enter key for submitting searched value
     const nameForm = useRef(null);
 
-    const handleSearch = () => {
+    const handleSearch = (rows, count) => {
         const form = nameForm.current;
         var changeableSearchedValue = `${form['searchvalue'].value}`;
         if (changeableSearchedValue === "") {
@@ -74,23 +81,32 @@ const SearchBar = (props) => {
         }
         props.changeSearchValue(changeableSearchedValue);
         props.changeBoxVisibility(true);
-        bodyParameters.name = changeableSearchedValue;
-        makeRequest();
+        if (rows) {
+            makeRequest(changeableSearchedValue, rows, count);
+        } else {
+            makeRequest(changeableSearchedValue, rowsPerPage, pageCount);
+        }
+
         return true;
     }
 
+    //vyhladanie po stlaceni Enteru, nastavi 10 zaznamov na stranu + nastavi prvu (0) stranu
     const handleKeyPress = (e) => {
+        setRowsPerPage(10);
+        setPageCount(0);
         if (e.keyCode == 13) {
-            if (!handleSearch()) {
+            if (!handleSearch(10, 0)) {
                 props.changeSearchValue("");
                 props.changeBoxVisibility(false);
             }
         }
     }
 
-    //handle click button for submitting searched value
+    //vyhladanie po kliknuti na tlacidlo, nastavi 10 zaznamov na stranu + nastavi prvu (0) stranu
     const handleClickEvent = () => {
-        if (!handleSearch()) {
+        setRowsPerPage(10);
+        setPageCount(0);
+        if (!handleSearch(10, 0)) {
             props.changeSearchValue("");
             props.changeBoxVisibility(false);
         }
@@ -119,18 +135,18 @@ const SearchBar = (props) => {
                 <Button onClick={handleClickEvent} variant="contained"
                         className={classes.searchButton}>Vyhľadaj</Button>
             </Grid>
-            {responseData ?<> <DataTreeView treeItems={responseData} className={classes.dataTreeStyles}/>
+            {responseData ? <> <DataTreeView treeItems={responseData} className={classes.dataTreeStyles}/>
                 <TablePagination
-                component="div"
-                count={responseData.numAllResults}
-                page={pageCount}
-                onPageChange={handleChangePage}
-                rowsPerPage={rowsPerPage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                labelRowsPerPage={"Strana"}
+                    component="div"
+                    count={responseData.numAllResults}
+                    page={pageCount}
+                    onPageChange={handleChangePage}
+                    rowsPerPage={rowsPerPage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    labelRowsPerPage={"Strana"}
                 />
 
-            </>: ""}
+            </> : ""}
         </Box>
     );
 }
