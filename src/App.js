5d60aed9e144f36243ab0b6f8e973e95f028a17d
dudakp <palodudak@gmail.com>
2022-03-02 12:33:59 +0100

    opraveny custom login

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import './App.css';
-import {LoginCallback, SecureRoute, Security} from "@okta/okta-react";
-import {OktaAuth, toRelativeUrl} from "@okta/okta-auth-js";
-import {Route, useHistory} from "react-router-dom";
-import MainPage from "./user/pages/main/MainPage";
-import WelcomePage from "./user/pages/welcome/WelcomePage";
-import LoginPage from "./user/pages/loginPage/LoginPage";
-import Login from "./Login"
+import {LoginCallback, SecureRoute, Security} from '@okta/okta-react';
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+import {Route, useHistory} from 'react-router-dom';
+import MainPage from './user/pages/main/MainPage';
+import WelcomePage from './user/pages/welcome/WelcomePage';
+import Login from './user/pages/loginPage/Login';
 
 function App() {
 
-    //login
-    const onAuthRequired = () => {
-        history.push('/login');
-    };
-    const history = useHistory(); // example from react-router
-    const oktaAuth = new OktaAuth({
-        issuer: process.env.REACT_APP_ISSUER,
-        clientId: process.env.REACT_APP_CLIENT_ID,
-        redirectUri: window.location.origin + '/login/callback',
-        onAuthRequired: onAuthRequired,
-        pkce: true
-    });
-    const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-        history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
-    };
+  //login
+  const onAuthRequired = () => {
+    history.push('/login');
+  };
+  const history = useHistory(); // example from react-router
+  const oktaAuth = new OktaAuth({
+    issuer: process.env.REACT_APP_ISSUER,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: window.location.origin + '/login/callback',
+    onAuthRequired: onAuthRequired,
+    pkce: true
+  });
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
 
-    return (
-        <Security
-            oktaAuth={oktaAuth}
-            restoreOriginalUri={restoreOriginalUri}
-        >
-            <Route path="/" exact={true} component={WelcomePage}/>
-            <Route path='/login' exact={true} component={Login}/>
-            {/*//SecureRoute obaluje security componenty*/}
-            <SecureRoute path="/ease" exact={true} component={MainPage}/>
-            <Route path='/registration' exact={true} component={LoginPage}/>
-            <Route path='/login/callback' component={LoginCallback}/>
+  return (
+    <Security
+      oktaAuth={oktaAuth}
+      restoreOriginalUri={restoreOriginalUri}
+    >
+      <Route path="/" exact={true} component={WelcomePage}/>
+      <Route path="/login" exact={true} component={Login}/>
+      {/*//SecureRoute obaluje security componenty*/}
+      <SecureRoute path="/ease" exact={true} component={MainPage}/>
+      <Route path="/login/callback" component={LoginCallback}/>
 
-        </Security>
-    );
+    </Security>
+  );
 }
 
 export default App;
