8d59fe3820cd99cff73d78acf517cfabbe7deee4
draganazrinji <dragana.zrinji@gmail.com>
2022-05-04 23:33:46 +0200

    feat: error handling

--- src/user/components/loginForm/OktaSignInWidget.jsx
+++ src/user/components/loginForm/OktaSignInWidget.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {withOktaAuth} from '@okta/okta-react';
-import {OktaAuth} from '@okta/okta-auth-js';
+import {isAuthApiError, OktaAuth} from '@okta/okta-auth-js';
 import {Button, Grid, TextField} from '@material-ui/core';
 import {withRouter} from 'react-router-dom';
-
 import {withStyles,useMediaQuery} from '@material-ui/styles';
+import {Alert} from "@material-ui/lab";
+
 
 const styles = theme => ({
     title: {
@@ -12,32 +13,44 @@ const styles = theme => ({
     },
     loginButton: {
         width: "100%",
-        align: "center"
+        align: "center",
+        paddingTop: "2%",
+        marginLeft: "33%"
     },
     box: {
-        padding: "1%",
-        width: "60%",
-        float: "right"
+        width: "500px",
+        margin : "auto",
+        padding: "16px"
     },
     text: {
         padding: "1%",
         marginBottom: "1%",
-        float:"center"
+        float:"center",
+        fontstyle: "bold"
+
     },
     between: {
         marginBottom: "1%"
     },
     fields: {
-        width: "30%"
+        width: "100%"
     },
     loginText: {
-        color: "grey"
+        color: "grey",
+        align: "center",
+        fontstyle: "bold",
+        marginLeft: "33%"
     },
     mainDiv: {
         backgroundImage: "url(/easeBackground.jpg)",
         height: "100vh",
         overflow: "hidden",
         backgroundSize: "cover"
+    },
+    alertmsg :{
+        width: "450px",
+        margin : "auto",
+        paddingLeft:"6px"
     }
 
 });
@@ -50,7 +63,9 @@ class OktaSignInWidget extends Component {
         this.state = {
             sessionToken: null,
             email: '',
-            password: ''
+            password: '',
+            hasError: false,
+            errorMessage: ''
         };
 
         this.oktaAuth = new OktaAuth({
@@ -71,22 +86,25 @@ class OktaSignInWidget extends Component {
     handlePasswordChange(event) {
         this.setState({password: event.target.value});
     }
-
     async signIn(event) {
         event.preventDefault();
         console.log(this.state.email);
         const transaction = await this.oktaAuth.signIn({
             username: this.state.email,
             password: this.state.password
+        }).catch(error => {
+            this.setState({ errorMessage: "Nesprávne prihlasovacie údaje." });
+            console.error( error);
         });
 
         if (transaction.status === 'SUCCESS') {
             this.props.oktaAuth.signInWithRedirect({sessionToken: transaction.sessionToken});
             const {history} = this.props;
+            this.state.hasError = false;
             history.push('/ease');
-        }else {
-
-            throw new Error('Could not sign in: ' + transaction.status);
+        }else{
+            this.state.hasError = true;
+            console.log(this.state.hasError)
         }
     }
 
@@ -94,19 +112,23 @@ class OktaSignInWidget extends Component {
         const {classes} = this.props;
         return (
             <Grid className={classes.mainDiv}>
+
             <Grid className={classes.box} item xs={12}>
 
                 <form onSubmit={this.signIn}>
-                    <Grid className={classes.loginText}>
+
+
+                    <Grid className={classes.loginText} is>
                         <h2>Prihlásenie</h2>
                     </Grid>
-
+                    { this.state.errorMessage &&
+                    <Alert className={classes.alertmsg} severity="error" > { this.state.errorMessage } </Alert>
+                    }
                     <Grid className={classes.between}>
                         <Grid className={classes.text}> Email </Grid>
                         <TextField className={classes.fields} variant="outlined" type="text" placeholder="Email"
                                    value={this.state.email} onChange={this.handleEmailChange} required/>
                     </Grid>
-
                     <Grid className={classes.between}>
                         <Grid className={classes.text}>Heslo </Grid>
                         <TextField className={classes.fields} variant="outlined" type="password" placeholder="Heslo"
@@ -117,8 +139,11 @@ class OktaSignInWidget extends Component {
                     <Grid className={classes.loginButton}>
                         <Button className="button" variant="contained" type="submit">Prihlásiť sa</Button>
                     </Grid>
+
                 </form>
+
             </Grid>
+
             </Grid>
         );
     }
